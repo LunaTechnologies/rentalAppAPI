6ca384fcab30d8a1e76d948276a1b192e8082263
Alexandru <alexbuzescu@yahoo.com>
2022-04-05 12:45:27 +0300

    Add project files.

---
+++ rentalAppAPI.DAL/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using rentalAppAPI.DAL.Configurations;
+using rentalAppAPI.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rentalAppAPI.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<test_tabel_01> test_tabels_01 { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new test_table_01Configuration());
+        }
+
+    }
+}
---
+++ rentalAppAPI.DAL/Migrations/20220405090500_AddedTestTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rentalAppAPI.DAL.Migrations
+{
+    public partial class AddedTestTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "test_tabels_01",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_test_tabels_01", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "test_tabels_01");
+        }
+    }
+}
