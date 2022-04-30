3fb96d9e7e0239f3b1bf855c6b831d4cd750d7e1
Alexandru Buzescu <39965333+AlexGabi17@users.noreply.github.com>
2022-04-30 14:47:53 +0300

    Update 20220430100908_AddPicturesRentalTypesServices.cs

--- rentalAppAPI.DAL/Migrations/20220430100908_AddPicturesRentalTypesServices.cs
+++ rentalAppAPI.DAL/Migrations/20220430100908_AddPicturesRentalTypesServices.cs
@@ -7,8 +7,6 @@ namespace rentalAppAPI.DAL.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "test_tabels_01");
 
             migrationBuilder.AddColumn<DateTime>(
                 name: "LastLogin",
@@ -141,19 +139,6 @@ namespace rentalAppAPI.DAL.Migrations
             migrationBuilder.DropColumn(
                 name: "Verified",
                 table: "AspNetUsers");
-
-            migrationBuilder.CreateTable(
-                name: "test_tabels_01",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_test_tabels_01", x => x.Id);
-                });
         }
     }
 }
